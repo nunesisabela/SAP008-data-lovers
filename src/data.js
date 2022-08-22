453976b8ee85778dbf552123fe73421773fec3a3
nunesisabela <isabelanunes.tech@gmail.com>
2022-08-22 18:05:14 -0300

    função filter select OK/ função filter name OK/ condições exibição de cards OK/ aside adicionado ao HTML

--- src/data.js
+++ src/data.js
@@ -2,7 +2,13 @@ export function filterData(array, property, value) { //filtra dados
   return array.results.filter(elem => elem[property] === value) //.filter somente acessa arrays. para acessar objetos, usamos "." (literal, como em .results)
 };
 
+export function filterName(array, characterCondition) { //filtra nome
+  return array.results.filter(elem => elem.name.toUpperCase().includes(characterCondition.toUpperCase())) 
+};
 
+export function sortData(array, property, order) {
+  array.sort
+}
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterData } from './data.js';
+import { filterData, filterName } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 const filterBox = document.querySelector('#filterbox');
@@ -6,66 +6,55 @@ filterBox.hidden = true;
 const buttonFilter = document.querySelector('#filter');
 const buttonSearch = document.querySelector('#search');
 const cards = document.querySelector('#cards');
+const keyWords = document.querySelector('#keywords');
 const form = document.querySelector('#form');
+const selectFilter = document.querySelector('#filteroptions');
+const calculation = document.querySelector('#calculation')
 
 
-buttonFilter.addEventListener('click', (event) => { //exibe filtros
+buttonFilter.addEventListener('click', (e) => { //exibe filtros
     filterBox.hidden = false;
-    event.preventDefault() //adicionar ao buttonSearch.addEventListener também
+    e.preventDefault();
+    form.reset(keyWords);
 });
 
-const keyWords = document.querySelector("#keywords").value //acessa valor do input de texto
-const genderChecked = document.querySelectorAll('input[name="gender"]:checked');
-const speciesChecked = document.querySelectorAll('input[name="species"]:checked');
-const statusChecked = document.querySelectorAll('input[name="status"]:checked');
-
-function showCards(infos) { //exibe cards
+function showCards(infos) { //cards template
     return infos.map((elem) => `
       <div class="cards">
       <img id="photos" src="${elem.image}" alt="imagem do personagem"><br>
       <p id="names">${elem.name}</p><br>
-        <ul class="cards-list">
-          <p>Status: </p>
-          <li>${elem.status}</li>
-          <p>Gênero: </p>
-          <li>${elem.gender}</li>
-          <p>Espécie: </p>
-          <li>${elem.species}</li>
-        </ul>
-        </div>
+      <ul class="cards-list">
+        <p>Status: </p>
+        <li>${elem.status}</li>
+        <p>Gênero: </p>
+        <li>${elem.gender}</li>
+        <p>Espécie: </p>
+        <li>${elem.species}</li>
+      </ul>
+      </div>
         `
     ).join('');
 };
 
 buttonSearch.addEventListener('click', (event) => { //filtra e exibe cards
-    event.preventDefault()
+    filterBox.hidden = true;
+    event.preventDefault();
+    calculation.innerHTML = 'olá';
 
-  
-     if(keyWords !== "") {
-        return console.log(data.map(elemento => (elemento.keyWords)))
-        
-    
-
-   
-    } else if (form.status[0].checked === true) {
-        return cards.innerHTML = showCards(filterData(data, "status", "Alive"))
-    } else if (form.status[1].checked === true) {
-        return cards.innerHTML = showCards(filterData(data, "status", "Dead"))
-    } else if (form.status[2].checked === true) {
-        returncards.innerHTML = showCards(filterData(data, "status", "unknown"))
-    } else if (form.gender[0].checked === true) {
-        return cards.innerHTML = showCards(filterData(data, "gender", "Female"))
-    } else if (form.gender[1].checked === true) {
-        return cards.innerHTML = showCards(filterData(data, "gender", "Male"))
-    } else if (form.species[0].checked === true) {
-        return cards.innerHTML = showCards(filterData(data, "species", "Human"))
-    } else if (form.species[1].checked === true) {
-        return cards.innerHTML = showCards(filterData(data, "species", ""))
+    const optionIndex = selectFilter.options[selectFilter.selectedIndex].value;
+    if (keyWords.value !== "") {
+        return cards.innerHTML = showCards(filterName(data, keyWords.value))
+    } else if (optionIndex === "Alive" || optionIndex === "Dead" || optionIndex === "unknown") {
+        return cards.innerHTML = showCards(filterData(data, "status", optionIndex))
+    } else if (optionIndex === "Female" || optionIndex === "Male" || optionIndex === "Genderless") {
+        return cards.innerHTML = showCards(filterData(data, "gender", optionIndex))
+    } else if (optionIndex === "all") {
+        return cards.innerHTML = showCards(data.results)
     };
 
+    // let percent = 1+1
 });
+
 // L29 E os personagens não-humanos? Como filtrá-los?
-// L44 Como puxar radio buttons pela class?
 // Como funciona o .join() depois do template?
-// CSS como scrollar apenas os cards na página?
 // Como usar as tipografias escolhidas?
