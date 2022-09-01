db5b43c3b02fccb70ec0d9b17fc980283ceda311
blericalopes <blericalopes@outlook.com>
2022-09-01 12:10:15 -0300

    testes sort passando

--- test/data.spec.js
+++ test/data.spec.js
@@ -73,7 +73,6 @@ describe('filters data by name', () => {
   it('is a function', () => {
     expect(typeof filterName).toBe('function');
   });
-
   it('should return object that contains the element from search', () => {
     const search = "rick"
     expect(filterName(data, search)).toStrictEqual([{
@@ -85,157 +84,6 @@ describe('filters data by name', () => {
   });
 });
 
-describe('sorts the array', () => {
-  it('is a function', () => {
-    expect(typeof sortArr).toBe('function');
-  });
-  it('should return characters sorted from A to Z', () => {
-    const arrOrderedAz = {
-      "info": {
-        "count": 6,
-        "pages": 1,
-      },
-      "results": [
-        {
-          "name": "Albert Einstein",
-          "status": "Dead",
-          "gender": "Male",
-          "episode": 5
-        },
-        {
-          "name": "Annie",
-          "status": "Alive",
-          "gender": "Female",
-          "episode": 2
-        },
-        {
-          "name": "Brad Anderson",
-          "status": "Dead",
-          "gender": "Male",
-          "episode": 3
-        },
-        {
-          "name": "Morty Smith",
-          "status": "Alive",
-          "gender": "Male",
-          "episode": 1
-        },
-        {
-          "name": "Rick Sanchez",
-          "status": "Alive",
-          "gender": "Male",
-          "episode": 4
-        },
-        {
-          "name": "Summer Smith",
-          "status": "Alive",
-          "gender": "Female",
-          "episode": 6
-        },
-      ]
-    }
-    expect(sortArr(data.results, 'az').toStrictEqual(arrOrderedAz))
-  })
-
-  it('should return characters sorted from Z to A', () => {
-    const arrOrderedZa = {
-      "info": {
-        "count": 6,
-        "pages": 1,
-      },
-      "results": [
-        {
-          "name": "Summer Smith",
-          "status": "Alive",
-          "gender": "Female",
-          "episode": 6
-        },
-        {
-          "name": "Rick Sanchez",
-          "status": "Alive",
-          "gender": "Male",
-          "episode": 4
-        },
-        {
-          "name": "Morty Smith",
-          "status": "Alive",
-          "gender": "Male",
-          "episode": 1
-        },
-        {
-          "name": "Brad Anderson",
-          "status": "Dead",
-          "gender": "Male",
-          "episode": 3
-        },
-        {
-          "name": "Annie",
-          "status": "Alive",
-          "gender": "Female",
-          "episode": 2
-        },
-        {
-          "name": "Albert Einstein",
-          "status": "Dead",
-          "gender": "Male",
-          "episode": 5
-        },
-      ]
-    }
-    const selectZa = 'za'
-    expect(sortArr(data, selectZa).toStrictEqual(arrOrderedZa))
-  })
-
-  it('should return characters ordered by episode', () => {
-    const arrOrderedEp = {
-      "info": {
-        "count": 6,
-        "pages": 1,
-      },
-      "results": [
-        {
-          "name": "Morty Smith",
-          "status": "Alive",
-          "gender": "Male",
-          "episode": 1
-        },
-        {
-          "name": "Annie",
-          "status": "Alive",
-          "gender": "Female",
-          "episode": 2
-        },
-        {
-          "name": "Brad Anderson",
-          "status": "Dead",
-          "gender": "Male",
-          "episode": 3
-        },
-        {
-          "name": "Rick Sanchez",
-          "status": "Alive",
-          "gender": "Male",
-          "episode": 4
-        },
-        {
-          "name": "Albert Einstein",
-          "status": "Dead",
-          "gender": "Male",
-          "episode": 5
-        },
-        {
-          "name": "Summer Smith",
-          "status": "Alive",
-          "gender": "Female",
-          "episode": 6
-        }
-      ]
-    }
-    const selectEpisode = 'episode'
-    expect(sortArr(data, selectEpisode)).toStrictEqual(arrOrderedEp)
-  })
-});
-
 describe('calculates percentage of the filtered characters', () => {
   it('is a function', () => {
     expect(typeof percentage).toBe('function');
@@ -244,3 +92,139 @@ describe('calculates percentage of the filtered characters', () => {
     expect(percentage(data, newArrObjFemale.length)).toBe(33);
   });
 })
+
+describe('sorts the array', () => {
+  it('is a function', () => {
+    expect(typeof sortArr).toBe('function');
+  });
+
+  it('should return characters sorted from A to Z', () => {
+    const arrOrderedAz = [
+      {
+        "name": "Albert Einstein",
+        "status": "Dead",
+        "gender": "Male",
+        "episode": 5
+      },
+      {
+        "name": "Annie",
+        "status": "Alive",
+        "gender": "Female",
+        "episode": 2
+      },
+      {
+        "name": "Brad Anderson",
+        "status": "Dead",
+        "gender": "Male",
+        "episode": 3
+      },
+      {
+        "name": "Morty Smith",
+        "status": "Alive",
+        "gender": "Male",
+        "episode": 1
+      },
+      {
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "gender": "Male",
+        "episode": 4
+      },
+      {
+        "name": "Summer Smith",
+        "status": "Alive",
+        "gender": "Female",
+        "episode": 6
+      },
+    ]
+    expect(sortArr(data.results, "az")).toStrictEqual(arrOrderedAz);
+  });
+
+
+  it('should return characters sorted from Z to A', () => {
+    const arrOrderedZa = [
+      {
+        "name": "Summer Smith",
+        "status": "Alive",
+        "gender": "Female",
+        "episode": 6
+      },
+      {
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "gender": "Male",
+        "episode": 4
+      },
+      {
+        "name": "Morty Smith",
+        "status": "Alive",
+        "gender": "Male",
+        "episode": 1
+      },
+      {
+        "name": "Brad Anderson",
+        "status": "Dead",
+        "gender": "Male",
+        "episode": 3
+      },
+      {
+        "name": "Annie",
+        "status": "Alive",
+        "gender": "Female",
+        "episode": 2
+      },
+      {
+        "name": "Albert Einstein",
+        "status": "Dead",
+        "gender": "Male",
+        "episode": 5
+      },
+    ]
+    expect(sortArr(data.results, "za")).toStrictEqual(arrOrderedZa);
+  });
+
+  it('should return characters ordered by episode', () => {
+    const arrOrderedEp = [
+      {
+        "name": "Morty Smith",
+        "status": "Alive",
+        "gender": "Male",
+        "episode": 1
+      },
+      {
+        "name": "Annie",
+        "status": "Alive",
+        "gender": "Female",
+        "episode": 2
+      },
+      {
+        "name": "Brad Anderson",
+        "status": "Dead",
+        "gender": "Male",
+        "episode": 3
+      },
+      {
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "gender": "Male",
+        "episode": 4
+      },
+      {
+        "name": "Albert Einstein",
+        "status": "Dead",
+        "gender": "Male",
+        "episode": 5
+      },
+      {
+        "name": "Summer Smith",
+        "status": "Alive",
+        "gender": "Female",
+        "episode": 6
+      }
+    ]
+
+
+    expect(sortArr(data.results, 'episode')).toStrictEqual(arrOrderedEp)
+  })
+});
+
